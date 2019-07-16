6c9e330d97a0392fb0ab0f8f38407be56ff8c442
olekhandro <olekhandro@rambler.ru>
2019-07-16 10:23:29 +0400

    Added users component

---
+++ AirsoftTeamApp/ClientApp/src/app/user-list/user-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'user-list-component',
+  templateUrl: './user-list.component.html'
+})
+export class UserListComponent {
+  isExpanded = false;
+
+}
